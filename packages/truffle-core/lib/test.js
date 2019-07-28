yxliang01/truffle

Pass config to setJSTestGlobals
For later access in closures

packages/truffle-core/lib/test.js
@@ -123,7 +123,13 @@ var Test = {
         );
       })
       .then(function() {
-        return self.setJSTestGlobals(web3, accounts, test_resolver, runner);
+        return self.setJSTestGlobals(
+          config,
+          web3,
+          accounts,
+          test_resolver,
+          runner
+        );
       })
       .then(function() {
         // Finally, run mocha.
@@ -231,7 +237,7 @@ var Test = {
     });
   },
 
-  setJSTestGlobals: function(web3, accounts, test_resolver, runner) {
+  setJSTestGlobals: function(config, web3, accounts, test_resolver, runner) {
     return new Promise(function(accept) {
       global.web3 = web3;
       global.assert = chai.assert;

liuis/truffle-conflux

packages/truffle-core/lib/test.js
@@ -106,7 +106,13 @@ const Test = {
       runner
     );
 
-    await this.setJSTestGlobals(web3, accounts, testResolver, runner);
+    await this.setJSTestGlobals(
+      config,
+      web3,
+      accounts,
+      testResolver,
+      runner
+    );
 
     // Finally, run mocha.
     process.on("unhandledRejection", reason => {
@@ -193,7 +199,7 @@ const Test = {
     });
   },
 
-  setJSTestGlobals: function(web3, accounts, testResolver, runner) {
+  setJSTestGlobals: function(config, web3, accounts, testResolver, runner) {
     return new Promise(function(accept) {
       global.web3 = web3;
       global.assert = chai.assert;