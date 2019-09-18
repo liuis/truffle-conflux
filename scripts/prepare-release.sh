446b7add5d133511808fb4b7d8c0c68bc58ef926
liuis <liuisblockchain@gmail.com>
2019-09-18 17:53:04 +0800

    try to publish

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -3,10 +3,10 @@
 # The below tells bash to stop the script if any of the commands fail
 set -ex
 
-LAST_PUBLISHED_TAG=$(awk -F\" '/"version":/ {print $4}' ./packages/truffle/package.json)
+LAST_PUBLISHED_TAG=$(awk -F\" '/"version":/ {print $4}' ./packages/truffle-conflux/package.json)
 
-git checkout develop
-git pull origin develop
+git checkout kill_ganancehe
+git pull origin kill_ganancehe
 yarn bootstrap
-prs-merged-since --repo trufflesuite/truffle --tag v$LAST_PUBLISHED_TAG --format markdown
+prs-merged-since --repo liuis/truffle-conflux --tag v$LAST_PUBLISHED_TAG --format markdown
 lerna changed
